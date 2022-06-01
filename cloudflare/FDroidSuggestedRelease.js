60e0999962d9f158212ab47729c841db4bfe0793
cvzi <cuzi@openmail.cc>
2022-06-01 13:20:35 +0200

    cache fetch for 60min

--- cloudflare/FDroidSuggestedRelease.js
+++ cloudflare/FDroidSuggestedRelease.js
@@ -2,9 +2,24 @@
 
 // https://f-droid-build.cuzi.workers.dev/{package}/
 
+const fetchConfig = {
+  cf: {
+    // Always cache this fetch regardless of content type
+    // for a max of 60min before revalidating the resource
+    cacheTtl: 60 * 60,
+    cacheEverything: true
+  }
+}
+const responseConfig = {
+  headers: {
+    'content-type': 'application/javascript;charset=UTF-8',
+    'source-code': 'github.com/cvzi/ScreenshotTile_miscellaneous/cloudflare/FDroidSuggestedRelease.js'
+  }
+}
+
 async function getLatestFdroidRelease (packageName) {
   const url = `https://f-droid.org/packages/${encodeURIComponent(packageName)}/`
-  const content = await (await fetch(url)).text()
+  const content = await (await fetch(url, fetchConfig)).text()
   const suggstedVersion = content.split('name="suggested"', 2)[1]
     .split('</div>', 2)[0]
     .split('name="', 3)
@@ -20,16 +35,11 @@ async function handleRequest (request) {
   const packageName = pathname.replace(/^\/|\/$/g, '').trim()
   if (packageName) {
     const latestVersion = await getLatestFdroidRelease(packageName)
-    return new Response(latestVersion, {
-      headers: {
-        'source-code': 'github.com/cvzi/ScreenshotTile_miscellaneous/cloudflare/FDroidSuggestedRelease.js'
-      }
-    })
+    return new Response(latestVersion, responseConfig)
   } else {
-    return new Response(`console.log("No package name found in '${request.url}'")`, {
-      'source-code': 'github.com/cvzi/ScreenshotTile_miscellaneous/cloudflare/FDroidSuggestedRelease.js',
+    return new Response(`console.log("No package name found in '${request.url}'")`, Object.assign({
       status: 400
-    })
+    }, responseConfig))
   }
 }
 
